32ba6a575208846e95e0314a8fe4852ced7f93d4
Pete Savage <psavage@redhat.com>
2018-03-21 21:51:34 +0000

    Added new work

--- hexa.py
+++ hexa.py
@@ -18,7 +18,8 @@ image_file_names = [
     "/home/user/06.jpeg",
 ]
 
-positions_f = [
+fp_data = [
+[
     (4, 1, 0),
     (5, 6, 180),
     (5, 2, -60),
@@ -37,72 +38,80 @@ positions_f = [
     (1, 9, 60),
     (4, 5, 0),
     (4, 3, -120)
+],
+[
+    (5, 3, -120)
 ]
+]
+im = Image.open(image_file_names[0])
+width, height = im.size
+base_image = Image.new("RGBA", (width * 18, height))
 
-base_image = None
 
-for i, im_place in enumerate(positions_f):
-    print im_place
-    segment, image, angle = im_place
+def run_tris(tris, row, offset):
+    for i, im_place in enumerate(tris):
+        print im_place
+        segment, image, angle = im_place
 
-    im = Image.open(image_file_names[image - 1])
-    width, height = im.size
-    trig_width = width / 2.0
-    trig_height = (trig_width / 2.0) * math.tan(math.radians(60))
+        im = Image.open(image_file_names[image - 1])
+        width, height = im.size
+        trig_width = width / 2.0
+        trig_height = (trig_width / 2.0) * math.tan(math.radians(60))
 
-    upper = (height / 2.0) - (trig_height)
-    lower = (height / 2.0) + (trig_height)
+        upper = (height / 2.0) - (trig_height)
+        lower = (height / 2.0) + (trig_height)
 
-    target_segs = {
-        0: 1,
-        -0: 1,
-        180: 4,
-        -180: 4,
-        -60: 2,
-        60: 6,
-        -120: 3,
-        120: 5,
-    }
+        target_segs = {
+            0: 1,
+            -0: 1,
+            180: 4,
+            -180: 4,
+            -60: 2,
+            60: 6,
+            -120: 3,
+            120: 5,
+        }
 
-    masks = [
-        # Segment 1
-        [(trig_width / 2.0, upper), (trig_width * 1.5, upper), (trig_width, height / 2.0)],
-        # Segment 2
-        [(trig_width, height / 2.0), (trig_width * 1.5, upper), (trig_width * 2.0, height / 2.0)],
-        # Segment 3
-        [(trig_width, height / 2.0), (trig_width * 2.0, height / 2.0), (trig_width * 1.5, lower)],
-        # Segment 4
-        [(trig_width, height / 2.0), (trig_width * 1.5, lower), (trig_width / 2.0, lower)],
-        # Segment 5
-        [(0, height / 2.0), (trig_width, height / 2.0), (trig_width / 2.0, lower)],
-        # Segment 6
-        [(0, height / 2.0), (trig_width, height / 2.0), (trig_width / 2.0, upper)]
-    ]
+        masks = [
+            [(trig_width / 2.0, upper), (trig_width *1.5, upper), (trig_width, height / 2.0)], #segment 1
+            [(trig_width, height / 2.0), (trig_width * 1.5, upper), (trig_width * 2.0, height / 2.0)], # segment 2
+            [(trig_width, height / 2.0), (trig_width * 2.0, height / 2.0), (trig_width * 1.5, lower)], # segment 3
+            [(trig_width, height / 2.0), (trig_width * 1.5, lower), (trig_width / 2.0, lower)], # segment 4
+            [(0, height / 2.0), (trig_width, height / 2.0), (trig_width / 2.0, lower)], # segment 5
+            [(0, height / 2.0), (trig_width, height / 2.0), (trig_width / 2.0, upper)] # segement 6
+        ]
 
-    coords = [
-        (trig_width / 2.0, upper, trig_width * 1.5, height / 2.0),
-        (trig_width, upper, trig_width * 2.0, height / 2.0),
-        (trig_width, height / 2.0, trig_width * 2.0, lower),
-        (trig_width / 2.0, height / 2.0, trig_width * 1.5, lower),
-        (0, height / 2.0, trig_width, lower),
-        (0, upper, trig_width, height / 2.0)
-    ]
+        coords = [
+            (trig_width / 2.0, upper, trig_width * 1.5, height / 2.0),
+            (trig_width, upper, trig_width * 2.0, height / 2.0),
+            (trig_width, height / 2.0, trig_width * 2.0, lower),
+            (trig_width / 2.0, height / 2.0, trig_width * 1.5, lower),
+            (0, height / 2.0, trig_width, lower),
+            (0, upper, trig_width, height / 2.0)
+        ]
 
-    target_seg = (target_segs[-angle] + segment) % 6 - 2
+        target_seg = (target_segs[-angle] + segment) % 6 - 2
+        print target_segs[angle], segment, target_segs[angle] + segment, (target_segs[angle] + segment) % 6, target_seg
 
-    al = im.copy().rotate(-angle)
-    compo = Image.new("RGBA", al.size, (0, 0, 0, 0))
-    mask = Image.new("RGBA", al.size, (0, 0, 0, 0))
+        al = im.copy().rotate(-angle)
+        compo = Image.new("RGBA", al.size, (0,0,0,0))
+        mask = Image.new("RGBA", al.size, (0,0,0,0))
 
-    draw = ImageDraw.Draw(mask, 'RGBA')
-    draw.polygon(masks[target_seg], (0, 0, 0, 255))
+        draw = ImageDraw.Draw(mask, 'RGBA')
+        draw.polygon(masks[target_seg], (0, 0, 0, 255))
 
-    compo.paste(al, (0, 0), mask)
-    compo.save('/tmp/{}compo.png'.format(i))
-    be = compo.crop(coords[target_seg])
-    be.save('/tmp/{}{}be.png'.format(image, i))
-    mask.save('/tmp/{}mask.png'.format(i))
-    if not base_image:
-        base_image = Image.new("RGBA", (width * 18, height))
-    base_image.paste(be, (int(trig_width) * i, 0))
+        compo.paste(al, (0, 0), mask)
+        compo.save('/tmp/{}compo.png'.format(i))
+        be = compo.crop(coords[target_seg])
+        be.save('/tmp/{}{}be.png'.format(image, i))
+        mask.save('/tmp/{}mask.png'.format(i))
+        t_offset = int(trig_width / 2) if offset == 1 else 0
+        base_image.paste(be, (int(trig_width) * i + t_offset, 0), be)
+
+offset = -1
+row = 0
+for data in fp_data:
+    run_tris(data, row, offset)
+    offset = offset * -1
+
 base_image.save("/tmp/moog.png")
